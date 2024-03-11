4e7be4d8c9006c96029ad46773ed5df045ad2f79
Henry <henryvictor215@gmail.com>
2024-03-11 21:25:56 +0100

    js tasks completed

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+//Basic print with js
+
+const myVar = 'JavaScript is amazing';
+
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+//printing using console.log
+
+console.log(“C is fun”);
+console.log(“Python is cool”);
+console.log(“JavaScript is amazing”);
